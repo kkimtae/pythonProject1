f95d982ef97aaa1e4b6b8ee156ec6993447665de
김형태 <kkimtae@gmail.com>
2022-06-18 01:54:49 +0900

    swagger 적용

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 from flask import Flask, jsonify, request
+from flask_restx import Api, Resource, reqparse
 import json
 
 server = Flask(__name__)
 
+api = Api(server, version='1.0', title='circle ci 연동 테스트', description='python api', doc='/api-docs')
 
-@server.route('/api1', methods=['POST'])
+api1_api = api.namespaces('제공 api', description='api1')
+api2_api = api.namespaces('제공 api', description='api2')
+
+@api1_api.route('/api1', methods=['POST'])
 def api1_request():
     print(request.get_json())
     d = {"result": {"status": "requested"}, "request_data": request.get_json()}
     json_data = json.dumps(d)
     return json_data, {"Content-Type": "application/json"}
 
-@server.route('/api2', methods=['POST'])
+
+@api2_api.route('/api2', methods=['POST'])
 def api2_request():
     print(request.get_json())
     d = {"result": {"status": "requested"}, "request_data": {"checkData1": "one"}}
